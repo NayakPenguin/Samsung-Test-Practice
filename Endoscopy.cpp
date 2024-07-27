aa5c33a9634b5fc730fd6061dbd283916e4f46db
Atanu Nayak <nayak.primary@gmail.com>
2024-07-27 13:45:07 +0530

    Create Endoscopy.cpp

---
+++ Endoscopy.cpp
@@ -0,0 +1,3 @@
+// Problem Statement:
+// You are given a 2D grid representing a maze where each cell can be open or blocked. You need to find the shortest path from the start cell to the target cell using BFS. The path can only traverse through open cells. If the target is unreachable, return -1.
+
