bea731df0729f9a7aa1620faf5bfef34272797b0
Atanu Nayak <nayak.primary@gmail.com>
2024-07-27 13:41:48 +0530

    Create BurstBalloon.cpp

---
+++ BurstBalloon.cpp
@@ -0,0 +1,7 @@
+// Problem Statement:
+// Given an array of integers nums representing balloons, where each balloon has a number of coins. You burst the balloons to maximize the coins you get. When you burst a balloon i, you get nums[left] * nums[i] * nums[right] coins, where left and right are the adjacent balloons of i. After the balloon is burst, the left and right become adjacent. Return the maximum coins you can collect by bursting the balloons wisely.
+
+// Solution : https://www.youtube.com/watch?v=VFskby7lUbw
+// https://www.youtube.com/watch?v=Yz4LlDSlkns
+
+// start from the end 
